bf6ad4070b13ddc2053a4a0049704778dba0e44e
Fireayehu Zekarias <fireayehuzekarias1989@gmail.com>
2020-10-18 22:51:52 +0300

    Transfer Commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/captain/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/captain/Downloads/Telegram Desktop/codecanyon_Tb7wBNIW_multi_restaurants_flutter_php_laravel_admin/flutter_application"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/home/captain/snap/flutter/common/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=2.3.0"
+export "FLUTTER_BUILD_NUMBER=2.3.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
